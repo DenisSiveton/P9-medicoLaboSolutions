4e48c783fbbb74b20a97e81f0c1c290eca45b2a5
Denis Siveton <denis.siveton@hotmail.com>
2024-05-27 19:56:37 +0200

    initial commit

---
+++ src/test/java/com/medicoLaboSolutions/backPatient/BackPatientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicoLaboSolutions.backPatient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackPatientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
