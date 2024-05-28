2514211ed91881a63a9c561ac01f06e9f8f0c9cb
Denis Siveton <denis.siveton@hotmail.com>
2024-05-28 13:55:49 +0200

    create patient controller and service for MVC approach

--- src/main/java/com/medicoLaboSolutions/backPatient/repository/PatientRepository.java
+++ src/main/java/com/medicoLaboSolutions/backPatient/repository/PatientRepository.java
@@ -4,7 +4,10 @@ import com.medicoLaboSolutions.backPatient.model.Patient;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PatientRepository extends CrudRepository<Patient, Integer> {
 
+    public Optional<Patient> findBySurname(String surname);
 }
