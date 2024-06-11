6dc524cfe4a24a58bfd9c55a7d6cfbfa3edb3d1f
Denis Siveton <denis.siveton@hotmail.com>
2024-06-11 18:44:18 +0200

    Refactor package name for better logic, security is included in the project configuration, not the other way around.

--- src/main/java/com/medicoLaboSolutions/backPatient/config/security/SpringSecurityConfig.java
+++ src/main/java/com/medicoLaboSolutions/backPatient/config/security/SpringSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.medicoLaboSolutions.backPatient.security.config;
+package com.medicoLaboSolutions.backPatient.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
