cb824b94e735143770f73361f3e081f4ab93346a
Denis Siveton <denis.siveton@hotmail.com>
2024-06-11 18:34:35 +0200

    Add validator and constraints to Patient for POST request

--- src/main/java/com/medicoLaboSolutions/backPatient/model/pojo/Patient.java
+++ src/main/java/com/medicoLaboSolutions/backPatient/model/pojo/Patient.java
@@ -1,7 +1,9 @@
-package com.medicoLaboSolutions.backPatient.model;
+package com.medicoLaboSolutions.backPatient.model.pojo;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.sql.Date;
 
@@ -21,31 +23,39 @@ public class Patient {
     private int patientId;
 
     @Column(name = "firstname")
+    @NotNull
+    @Pattern(regexp ="^[a-zA-Z]+$")
     private String firstname;
 
     @Column(name = "lastname")
+    @NotNull
+    @Pattern(regexp ="^[a-zA-Z]+$")
     private String lastname;
 
     @Column(name = "birth_date")
+    @DateTimeFormat(pattern = "YYYY-MM-dd")
     private Date birthDate;
 
     @Column(name = "gender")
-    private char gender;
+    @Pattern(regexp ="^[F|M]{1}$")
+    @Size(min = 1, max = 1)
+    private String gender;
 
     @Column(name = "address")
     private String address;
 
     @Column(name = "phone_number")
+    @Pattern(regexp = "^\\(?([0-9]{3})\\)?[-]([0-9]{3})[-]([0-9]{4})$")
     private String phoneNumber;
 
-    public Patient(String firstname, String lastname, Date birthDate, char gender) {
+    public Patient(String firstname, String lastname, Date birthDate, String gender) {
         this.firstname = firstname;
         this.lastname = lastname;
         this.birthDate = birthDate;
         this.gender = gender;
     }
 
-    public Patient(String firstname, String lastname, Date birthDate, char gender, String address, String phoneNumber) {
+    public Patient(String firstname, String lastname, Date birthDate, String gender, String address, String phoneNumber) {
         this.firstname = firstname;
         this.lastname = lastname;
         this.birthDate = birthDate;
