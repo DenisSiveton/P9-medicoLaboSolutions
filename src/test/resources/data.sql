d3d5bc9b2e50a949f0e4df21ec1d4b169b20bcb1
Denis Siveton <denis.siveton@hotmail.com>
2024-06-11 18:47:40 +0200

    Implement CRUD unit test for Patient repository. Add H2 database for test purposes. data.sql is added for data entries.

---
+++ src/test/resources/data.sql
@@ -0,0 +1,4 @@
+insert into patient(firstname, lastname, birth_date, gender, address, phone_number) values ('Test','TestNone','1966-12-31','F','1 Brookside St','100-222-3333');
+insert into patient(firstname, lastname, birth_date, gender, address, phone_number) values ('Test','TestBorderline','1945-06-24','M','2 High St','200-333-4444');
+insert into patient(firstname, lastname, birth_date, gender, address, phone_number) values ('Test','TestInDanger','2004-06-18','M','3 Club Road','300-444-5555');
+insert into patient(firstname, lastname, birth_date, gender, address, phone_number) values ('Test','TestEarlyOnset','2002-06-28','F','4 Valley Dr','400-555-6666');
