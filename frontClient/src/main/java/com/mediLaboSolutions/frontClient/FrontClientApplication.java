a39aea4519037867b9d3b29c08504df51768a5c2
Denis Siveton <denis.siveton@hotmail.com>
2024-06-14 23:37:48 +0200

    Add bootstrap and feign to the project

--- frontClient/src/main/java/com/mediLaboSolutions/frontClient/FrontClientApplication.java
+++ frontClient/src/main/java/com/mediLaboSolutions/frontClient/FrontClientApplication.java
@@ -2,7 +2,9 @@ package com.mediLaboSolutions.frontClient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients("com.medicoLaboSolutions.frontClient")
 @SpringBootApplication
 public class FrontClientApplication {
 
