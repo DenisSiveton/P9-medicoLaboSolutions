35b5b499e2846ec80465207aa689b539817e8c43
Denis Siveton <denis.siveton@hotmail.com>
2024-06-15 00:01:35 +0200

    Create the proxy for the backPatient microservice and update the server port for each microservices. 9000 for frontClient 9001 for backPatient

---
+++ frontClient/src/main/java/com/mediLaboSolutions/frontClient/proxies/MicroservicePatientProxy.java
@@ -0,0 +1,16 @@
+package com.mediLaboSolutions.frontClient.proxies;
+
+import com.mediLaboSolutions.frontClient.beans.PatientBean;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name ="backPatient", url ="localhost:9001")
+public interface MicroservicePatientProxy {
+    @GetMapping(value = "/patients")
+    ResponseEntity<Iterable<PatientBean>> getAllPatients();
+
+    @GetMapping( value = "/patients/{lastname}")
+    ResponseEntity<PatientBean> getPatient(@PathVariable("lastname") String lastname);
+}
