8e6b41f2f6c85047b38d2eaa4e15145235d7d856
Denis Siveton <denis.siveton@hotmail.com>
2024-06-14 22:29:18 +0200

    Import front client microservice as a new module with isolated pom.xml

---
+++ frontClient/src/test/java/com/mediLaboSolutions/frontClient/FrontClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediLaboSolutions.frontClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
