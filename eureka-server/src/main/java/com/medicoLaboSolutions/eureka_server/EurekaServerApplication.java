f59f8b9aa10a760b0c56d7a89d2b8daddc2b6613
Denis Siveton <denis.siveton@hotmail.com>
2024-07-11 17:21:55 +0200

    update package name

--- eureka-server/src/main/java/com/medicoLaboSolutions/eureka_server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/medicoLaboSolutions/eureka_server/EurekaServerApplication.java
@@ -1,8 +1,7 @@
-package com.medicoLaboSolutions.eureka;
+package com.medicoLaboSolutions.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
--- eureka-server/src/test/java/com/medicoLaboSolutions/eureka_server/EurekaApplicationTests.java
+++ eureka-server/src/test/java/com/medicoLaboSolutions/eureka_server/EurekaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.medicoLaboSolutions.eureka;
+package com.medicoLaboSolutions.eureka_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
