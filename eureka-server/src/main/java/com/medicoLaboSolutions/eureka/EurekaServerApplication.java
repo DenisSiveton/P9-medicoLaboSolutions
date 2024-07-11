b5a422765cfc92431da37d094057f689ff037ed8
Denis Siveton <denis.siveton@hotmail.com>
2024-07-11 14:13:56 +0200

    update pom.xml add BackPatientApplication into eureka discovery microservices

--- eureka-server/src/main/java/com/medicoLaboSolutions/eureka/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/medicoLaboSolutions/eureka/EurekaServerApplication.java
@@ -2,14 +2,15 @@ package com.medicoLaboSolutions.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaApplication {
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
 }
