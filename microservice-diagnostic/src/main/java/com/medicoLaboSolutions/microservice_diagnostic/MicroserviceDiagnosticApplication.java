a0f46829bb7b5e2eb994cd4e8f75d89879143f31
Denis Siveton <denis.siveton@hotmail.com>
2024-09-02 13:44:22 +0200

    import new module for microservice diagnostic

---
+++ microservice-diagnostic/src/main/java/com/medicoLaboSolutions/microservice_diagnostic/MicroserviceDiagnosticApplication.java
@@ -0,0 +1,13 @@
+package com.medicoLaboSolutions.microservice_diagnostic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceDiagnosticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceDiagnosticApplication.class, args);
+	}
+
+}
