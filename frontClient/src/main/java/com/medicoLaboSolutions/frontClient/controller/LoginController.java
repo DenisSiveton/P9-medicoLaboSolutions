ff050327ed764113f6232e2c860392f9b2980e54
Denis Siveton <denis.siveton@hotmail.com>
2024-10-03 15:26:27 +0200

    Refactor spring security into FrontClientApplication side. Remove security from BackPatientApplication side.
    Add LoginController and login.html page to authenticate.
    
    Add data.sql to populate the database with default users.
    
    update pom.xml for backPatient and frontClient.

---
+++ frontClient/src/main/java/com/medicoLaboSolutions/frontClient/controller/LoginController.java
@@ -0,0 +1,12 @@
+package com.medicoLaboSolutions.frontClient.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    String login() {
+        return "login";
+    }
+}
